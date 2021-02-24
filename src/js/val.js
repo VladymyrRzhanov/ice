e8d39d4641a8606df5206f9c42f26ea189073e0e
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-24 18:10:52 +0200

    ccc

---
+++ src/js/val.js
@@ -0,0 +1,12 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function checkParams() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var phone = $('#phone').val();
+
+    if (name.length != 0 && email.length >= 6 && phone.length >= 10) {
+        $('#submit').removeAttr('disabled');
+    } else {
+        $('#submit').attr('disabled', 'disabled');
+    }
+})()
